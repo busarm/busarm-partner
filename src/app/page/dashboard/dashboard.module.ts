2ecc9723a445932436e5f0eb8435bdafe017113f
Samuel Gabriel <splendy5@gmail.com>
2020-10-14 23:37:08 +0800

    Fixed booking page not loaded bug

--- src/app/page/dashboard/dashboard.module.ts
+++ src/app/page/dashboard/dashboard.module.ts
@@ -5,15 +5,25 @@ import {FormsModule} from '@angular/forms';
 import {IonicModule} from '@ionic/angular';
 
 import {DashboardPage} from './dashboard.page';
+import { ViewBookingPage } from '../bookings/view-booking/view-booking.page';
+import { ViewBookingPageModule } from '../bookings/view-booking/view-booking.module';
+import { ViewTripPageModule } from '../trip/view-trip/view-trip.module';
+import { ViewTripPage } from '../trip/view-trip/view-trip.page';
 
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        IonicModule
+        IonicModule,
+        ViewBookingPageModule,
+        ViewTripPageModule
     ],
-    declarations: [DashboardPage] 
+    declarations: [DashboardPage],
+    entryComponents: [
+        ViewBookingPage,
+        ViewTripPage
+    ]
 })
 export class DashboardPageModule {
 }
