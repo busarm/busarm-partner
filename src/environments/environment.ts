473ba582f4ab18cc4578711303426945bef5bae9
Samuel Gabriel <splendy5@gmail.com>
2021-01-19 04:57:08 +0800

    Fixed wrong version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const ENVIRONMENT: ENV = ENV.DEV;
 export const CONFIGS = {
     production: false,
     app_name: "Wecari Partner",
-    app_version: "1.0.27", 
+    app_version: "1.0.28", 
     oauth_client_id: "wecari_partner_56823450923",
     oauth_client_secret: "15f625e7a398e734ed02859f3a338829",
     bugsnag_key: "",
