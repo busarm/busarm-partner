3b6156ff609bb1239e5e62bfe650273050198685
Samuel Gabriel <splendy5@gmail.com>
2021-02-05 12:53:53 +0800

    Updated version

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const ENVIRONMENT: ENV = location.host.match(/staging\./) ? ENV.TEST : EN
 export const CONFIGS = {
     production: true,
     app_name: "Wecari Partner",
-    app_version: "1.0.39",
+    app_version: "1.0.40",
     oauth_client_id: "wecari_partner_app_M4NgNbnsCy",
     oauth_client_secret: "2627b1c45ef96be17b7a3de1cd4d3bad5172b1381f2b2100e41edebc68ec42e9",
     oauth_scopes: ENVIRONMENT == ENV.TEST ? ['openid', 'user', 'agent', 'tester'] :  ['openid', 'user', 'agent'],
