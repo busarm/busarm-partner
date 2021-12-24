81ffcf2046d26ee1c713744ac0727647cb576a0a
Samuel Gabriel <splendy5@gmail.com>
2021-12-25 02:32:39 +0800

    Update env, add default scopes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const CONFIGS = {
     production: true,
     app_name: "Wecari Partner",
     app_version: "1.0.53", 
+    oauth_scopes: ENVIRONMENT == ENV.TEST ? ['openid', 'user', 'agent', 'tester'] :  ['openid', 'user', 'agent'],
     ...envConfigs
 };
 
