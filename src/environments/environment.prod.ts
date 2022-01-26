cd4e6e5e1cb6ed561a7cdc8fb0f5779026fe053b
Samuel Gabriel <splendy5@gmail.com>
2022-01-26 13:31:54 +0800

    Updated search bar search event to use enter key event rather

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const ENVIRONMENT: ENV = (envConfigs.testing || location.host.match(/stag
 export const CONFIGS = {
     production: true,
     app_name: "Wecari Partner",
-    app_version: "1.0.55", 
+    app_version: "1.0.56", 
     oauth_scopes: ENVIRONMENT == ENV.TEST ? ['openid', 'user', 'agent', 'tester'] :  ['openid', 'user', 'agent'],
     ...envConfigs
 };
