445ba799a2e9adbc9501053a6c9d7e6f25a6f06c
Samuel Gabriel <splendy5@gmail.com>
2022-04-13 23:15:05 +0800

    Replaced Oauth client with npm library

--- src/app/page/hooks/oauth/authorize/authorize.page.ts
+++ src/app/page/hooks/oauth/authorize/authorize.page.ts
@@ -1,22 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+
+import { OauthStorage } from 'busarm-oauth-client-js';
+
 import { PageController } from '../../../../page/page-controller';
 import { ToastType, Utils } from '../../../../helpers/Utils';
-import { OauthStorage } from '../../../../helpers/Oauth';
 import { Urls } from '../../../../helpers/Urls';
 import { Strings } from '../../.../../../../resources';
- 
+
 @Component({
   selector: 'app-authorize',
   templateUrl: './authorize.page.html',
   styleUrls: ['./authorize.page.scss'],
 })
 export class AuthorizePage extends PageController {
- 
+
   constructor() {
     super();
   }
 
-  
+
   public async ngOnInit() {
     await super.ngOnInit();
     let params = await this.getQueryParams()
